1c676a01b159c3181f34e0799e1cc4e2decab8b5
Jermin Amartey <jerminamartey@gmail.com>
2024-09-13 14:23:23 +0000

    additional database fiels

--- api/models.py
+++ api/models.py
@@ -5,6 +5,7 @@ class Students(models.Model):
     email = models.EmailField(default="email@ymail.com")
     name = models.CharField(max_length=100)
     cohort = models.CharField(max_length=100, null=False)
+    class_attended = models.DecimalField(decimal_places=5, max_digits=10)
     
     
     def __str__(self) -> str:
