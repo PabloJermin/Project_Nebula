80be75f37f672e02f82073ae14a5a07681dd9e94
Jermin Amartey <jerminamartey@gmail.com>
2024-09-13 17:21:11 +0000

    updated database fields

--- api/models.py
+++ api/models.py
@@ -5,7 +5,8 @@ class Students(models.Model):
     email = models.EmailField(default="email@ymail.com")
     name = models.CharField(max_length=100)
     cohort = models.CharField(max_length=100, null=False)
-    class_attended = models.DecimalField(decimal_places=5, max_digits=10)
+    class_attendance = models.DecimalField(decimal_places=5, max_digits=10)
+    average_score = models.DecimalField(decimal_places=3, max_digits=10)
     
     
     def __str__(self) -> str:
